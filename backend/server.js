ba3f7622981850dad8f0d6b182d7857f3d484060
Matthew Yoon <myoon11@my.bcit.ca>
2023-11-17 16:52:27 -0800

    please work

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,16 @@
 const express = require("express")
 const app = express()
-
+port = 3000
 const cors = require("cors")
 app.use(cors())
 
-app.listen(3000, () =>
+app.listen(port, () =>
     console.log("3000 works")
 )
+// app.get("/", (req, resp) => {
+//     res.sendFile("C:/Users/myoon/github-classroom/comp1537-f23-classroom/lab9-weather-app-node-js-and-express-js-matthewy0987/frontend/index.html");
+// })
+
 app.get("/weather", async (req, res) => {
     cityName = req.query.city
     resp = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=a7d9a0d099cf4d8d11b976e2239c1121`)
--- frontend/index.js
+++ frontend/index.js
@@ -1,6 +1,7 @@
 document.getElementById("weatherBtn").addEventListener("click", async () => {
     let cityName = document.getElementById("cname").value
-    resp = await fetch(`http://localhost:3000/weather?city=${cityName}&units=metric`)
+    // resp = await fetch(`http://localhost:3000/weather?city=${cityName}&units=metric`)
+    resp = await fetch(`https://comp1537-lab09-backend-2.onrender.com/weather?city=${cityName}&units=metric`)
     resJson = await resp.json()
     document.getElementById("result").innerText =
         resJson.weather[0].description
